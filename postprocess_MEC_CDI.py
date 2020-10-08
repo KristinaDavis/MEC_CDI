85add0c65a354726909abc18052197a6bb86aaae
captain-kay <k_davis@physics.ucsb.edu>
2020-10-08 12:01:17 -0700

    proper real/imag fft phase interpolation for DeltaP from dm coords to MEC coords for probe pair set

--- postprocess_MEC_CDI.py
+++ postprocess_MEC_CDI.py
@@ -1,3 +1,4 @@
+##
 """
 postprocess_MEC_CDI
 Kristina Davis
@@ -7,18 +8,19 @@ Aug 26 2020
 """
 
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib.colors import LogNorm, SymLogNorm
 import os
-import pickle
 import time
 import datetime
-import pytz
+import pickle
 import scipy.linalg as sl
+from scipy import interpolate
+from matplotlib import pyplot as plt
+from matplotlib.colors import LogNorm, SymLogNorm
 
-from mec_cdi import CDI_params, Slapper
+from mec_cdi import Slapper
+from read_photons import open_MEC_tseries
 
-
+##
 def cdi_postprocess(fp_seq, meta, plot=False):
     """
     this is the function that accepts the timeseries of intensity images from the simulation and returns the processed
@@ -26,41 +28,64 @@ def cdi_postprocess(fp_seq, meta, plot=False):
     hole over the specified region of the image.
 
     :param fp_seq: focal plane sequence (temporal cube) [nx, ny, time]
-    :param sampling: focal plane sampling
-    :return:
+    :param meta: metadata from .pkl file from DM settings and timestream created at runtime
+    :param plot: toggle to display plots or not
+    :return: nothing
     """
+    ##
     print(f'\nStarting CDI post-processing')
     st = time.time()
 
     nx = fp_seq.shape[0]
     ny = fp_seq.shape[1]
+    dm_act = meta.probe.DM_cmd_cycle.shape[-1]
 
     # Defining Matrices
     n_pairs = meta.ts.n_probes // 2  # number of deltas (probe differentials)
-    n_nulls = 4  #meta.ts.null_time *meta.ts.
+    n_nulls = np.int(meta.ts.null_time / meta.ts.phase_integration_time)
     delta = np.zeros((n_pairs, nx, ny), dtype=float)
+    phs_delta = np.zeros((n_pairs*2, nx, ny), dtype=float)
     Epupil = np.zeros((n_nulls*2, nx, ny), dtype=complex)
     H = np.zeros((n_pairs, 2), dtype=float)
     b = np.zeros((n_pairs, 1))
 
-    # Differance Images (delta)
+    ## Differance Images (delta)
     for ip in range(n_pairs):
         delta[ip] = np.copy(fp_seq[:,:,ip] - fp_seq[:,:,ip + n_pairs])
+        # FFT and Interpolate DM Map onto MEC coordinates
+        fftA = (1/np.sqrt(2*np.pi) *\
+                                   np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(meta.probe.DM_cmd_cycle[ip]))))
+        fftB = (1/np.sqrt(2*np.pi) *\
+                                   np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(meta.probe.DM_cmd_cycle[ip+n_pairs]))))
+        Ar = interpolate.interp2d(range(dm_act), range(dm_act), fftA.real, kind='cubic')
+        Ai = interpolate.interp2d(range(dm_act), range(dm_act), fftA.imag, kind='cubic')
+        Br = interpolate.interp2d(range(dm_act), range(dm_act), fftB.real, kind='cubic')
+        Bi = interpolate.interp2d(range(dm_act), range(dm_act), fftB.imag, kind='cubic')
+        ArI = Ar(np.linspace(0,dm_act,ny), np.linspace(0,dm_act, nx))
+        AiI = Ai(np.linspace(0,dm_act,ny), np.linspace(0,dm_act, nx))
+        BrI = Br(np.linspace(0,dm_act,ny), np.linspace(0,dm_act, nx))
+        BiI = Bi(np.linspace(0,dm_act,ny), np.linspace(0,dm_act, nx))
+        phs_delta[ip,:,:] = np.arctan2(AiI-BiI, ArI-BrI)
 
+
+    # for ip in range(meta.ts.n_probes):
+    #     phs_fft = np.angle(1/np.sqrt(2*np.pi) *\
+    #                                np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(meta.probe.DM_cmd_cycle[ip]))))
+    #     f = interpolate.interp2d(range(phs_fft.shape[0]), range(phs_fft.shape[0]), phs_fft, kind='cubic')
+    #     ipd = f(np.linspace(0,phs_fft.shape[0],ny), np.linspace(0,phs_fft.shape[0], nx))
+    #     phs_delta[ip,:,:] = np.roll(np.roll(ipd,30,axis=0),10,axis=1)
+##
     for i in range(nx):
             for j in range(ny):
 
                 for xn in range(n_nulls-1):
                     for ip in range(n_pairs):
-                        # print(f'\nip={ip}, xn={xn}, n_nulls = {n_nulls} i={i}, j={j}')
-                        # print(f'first={[i,j,ip]}, second={[i,j,ip + n_pairs]}, third={[i,j,meta.ts.n_probes+xn]}')
                         absDeltaP = np.abs((fp_seq[i,j,ip] + fp_seq[i,j,ip + n_pairs]) / 2
                                            - fp_seq[i,j,meta.ts.n_probes+xn])
                         absDeltaP = np.sqrt(absDeltaP)
-                        # probe_ft = (1/np.sqrt(2*np.pi)) *
-                        # np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(cdi.cout.DM_probe_series[ix])))
-                        phsDeltaP = np.arctan2(fp_seq[i,j,ip].imag - fp_seq[i,j, meta.ts.n_probes+xn].imag,
-                                               fp_seq[i,j,ip].real - fp_seq[i,j, meta.ts.n_probes+xn].real)
+                        phsDeltaP = phs_delta[ip,i,j]
+                        # phsDeltaP = np.arctan2(fp_seq[i,j,ip].imag - fp_seq[i,j, meta.ts.n_probes+xn].imag,
+                        #                        fp_seq[i,j,ip].real - fp_seq[i,j, meta.ts.n_probes+xn].real)
                         cpxDeltaP = absDeltaP*np.exp(1j*phsDeltaP)
 
                         H[ip, :] = [-cpxDeltaP.imag, cpxDeltaP.real]
@@ -73,8 +98,27 @@ def cdi_postprocess(fp_seq, meta, plot=False):
 
     et = time.time()
     print(f'\tCDI post-processing for {n_nulls} null-images finished in {et-st:.1f} sec == {(et-st)/60:.2f} min')
-
+##
     if plot:
+        ########################
+        # Phase Delta Images
+        fig, subplot = plt.subplots(1, n_pairs, figsize=(14, 5))
+        fig.subplots_adjust(wspace=0.5, right=0.85)
+
+        fig.suptitle('Phase Interpolated')
+
+        for ax, ix in zip(subplot.flatten(), range(n_pairs)):
+            im = ax.imshow(phs_delta[ix] * 1e6, interpolation='none', origin='lower',
+                           # norm=SymLogNorm(linthresh=1e-2),
+                           vmin=-1, vmax=1)  # , norm=SymLogNorm(linthresh=1e-5))
+            ax.set_title(f"Phase\n" + r'$\theta$' + f'={meta.ts.phase_cycle[ix] / np.pi:.3f}' +
+                         r'$\pi$ -$\theta$' + f'={meta.ts.phase_cycle[ix + n_pairs] / np.pi:.3f}' + r'$\pi$')
+
+        # cax = fig.add_axes([0.9, 0.2, 0.03, 0.6])  # Add axes for colorbar @ position [left,bottom,width,height]
+        # cb = fig.colorbar(im, orientation='vertical', cax=cax)  #
+        # cb.set_label('Intensity')
+
+        plt.show()
         ####################
         # Difference Images
         fig, subplot = plt.subplots(1, n_pairs, figsize=(14,5))
@@ -97,13 +141,13 @@ def cdi_postprocess(fp_seq, meta, plot=False):
 
         ##############################
         # Focal Plane E-field Estimate
-        fig, subplot = plt.subplots(1, n_nulls, figsize=(14, 5))
+        fig, subplt = plt.subplots(1, 3, figsize=(14, 5))
         fig.subplots_adjust(wspace=0.5, right=0.85)
 
         fig.suptitle('Estimated Focal-Plane E-field')
-
-        for ax, ix in zip(subplot, range(n_nulls)):
-            im = ax.imshow(np.abs(Epupil[ix])**2 * 1e6, interpolation='none', origin='lower')#,
+        for ax, ix in zip(subplt, range(3)):
+            print(f'ax={ax}, ix={ix}')
+            im = ax.imshow(np.abs(Epupil[ix])**2 * 1e6, interpolation='none', origin='lower')  #,
                            # norm=LogNorm())
             ax.set_title(f'Estimation timestep {ix}')
 
@@ -115,7 +159,7 @@ def cdi_postprocess(fp_seq, meta, plot=False):
 
         # #####################
         # Subtracted E-field
-        fig, subplot = plt.subplots(1, n_nulls, figsize=(14, 5))
+        fig, subplot = plt.subplots(2, 5, figsize=(14, 5))
         fig.subplots_adjust(wspace=0.05, right=0.85)
 
         fig.suptitle('Subtracted E-field')
@@ -133,12 +177,10 @@ def cdi_postprocess(fp_seq, meta, plot=False):
 
 
 if __name__ == '__main__':
-    # dm_header = open_MEC_tseries('/home/captainkay/mazinlab/MEC_data/CDI_tseries3-9-2020_hour0_min39.pkl')
-    dm_meta = open_MEC_tseries('/work/kkdavis/scratch/CDI_tseries_3-9-2020_hour0_min11.pkl')
-
-    print(f'First Timestep = {first_tstep(dm_meta):.0f}\nLast Timestep = {last_tstep(dm_meta):.0f}')
-
-    fp_seq = np.load('cdi1_timeCube3.npy', allow_pickle=True)
+    dm_meta = open_MEC_tseries('CDI2/CDI_tseries_10-6-2020_hour5_min27.pkl')
+    # dm_meta = open_MEC_tseries('/work/kkdavis/scratch/old/CDI_tseries_3-9-2020_hour0_min11.pkl')
+    # data = np.load('CDI2/CDI2_config_1601962002.npz', allow_pickle=True)
+    fp_seq = np.load('CDI2_tcube_fullcycle_1601962002.npy', allow_pickle=True)
     grail = cdi_postprocess(fp_seq, dm_meta, plot=True)
 
-    dumm=0
+    dumm=1
